ae10b9c6c0f910aae5ba9de1026c9698accee465
Eric Silva <eric.fernando@pitang.com>
2022-11-12 17:59:50 -0300

    Atualização angular

--- Front/ProEventos-App/src/app/nav/nav.component.ts
+++ Front/ProEventos-App/src/app/nav/nav.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   isCollapsed = true;
-  constructor() { }
+  constructor() { /* TODO document why this constructor is empty */  }
 
   ngOnInit() {
+    // TODO document why this method 'ngOnInit' is empty
+
   }
 
 }
