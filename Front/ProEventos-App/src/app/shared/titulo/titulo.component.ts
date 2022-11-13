379d02fc9398ee4a6a9dd30638affc46f74d100d
Eric Silva <eric.fernando@pitang.com>
2022-11-13 18:10:38 -0300

    inclusão de header e correção extensões css 2 scss

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -3,13 +3,15 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
-  styleUrls: ['./titulo.component.css']
+  styleUrls: ['./titulo.component.scss']
 })
 
 export class TituloComponent implements OnInit {
 
-  @Input()
-  titulo!: string;
+  @Input() titulo = 'Pro Eventos';
+  @Input() iconClass = 'fa fa-user';
+  @Input() subtitulo = 'Desde 2021';
+  @Input() botaoListar = false;
 
   constructor() { /*TODO document why this constructor is empty */}
 
