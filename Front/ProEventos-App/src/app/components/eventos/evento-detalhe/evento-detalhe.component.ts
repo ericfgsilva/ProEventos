204333d0a571e5b0cd1d710ed7cb19951a1d8b50
Eric Silva <eric.fernando@pitang.com>
2022-11-14 20:24:45 -0300

    Preparação formulário eventos

--- Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventoDetalheComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+
+  constructor() {
+    this.formulario = new FormGroup({})
+  }
 
   ngOnInit(): void {
+    this.validation();
+
   }
+
+  public validation(): void{
+    this.formulario = new FormGroup({
+      tema: new FormControl('',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(50)]
+      ),
+      local: new FormControl('', Validators.required),
+      dataEvento: new FormControl('',
+        [Validators.required, Validators.maxLength(120000)]
+      ),
+      qtdPessoas: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('',
+        [Validators.required, Validators.email]
+      ),
+      imageURL: new FormControl('', Validators.required),
+      imageAlt: new FormControl('', Validators.required),
+    })
+  }
+
 
 }
