45062e032352d8a5e90822ff03be0919124dd6e3
Eric Silva <eric.fernando@pitang.com>
2022-11-17 21:35:32 -0300

    conclusão formuláriode cadastro de usuario

--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  formulario!: FormGroup;
+
+  get form(): any {
+    return this.formulario.controls;
+  }
+
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validation();
+  }
+
+  private validation(): void{
+    
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmeSenha')
+    };
+
+    this.formulario = this.fb.group({
+      primeiroNome: ['', [Validators.required, Validators.minLength(2)]],
+      ultimoNome: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      usuario: ['', [Validators.required, Validators.minLength(4)]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmeSenha: ['', Validators.required],
+    }, formOptions);
+  }
+
+  public resetForm(): void {
+    this.formulario.reset();
   }
 
 }
